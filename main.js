b570d0848630592004c9b4fad998053b64912624
Violet <110205356+Violets-puragtory@users.noreply.github.com>
2023-03-27 15:45:20 +0000

    Hey Louis pimp this one

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let activebutton = null
-let level = 1
+let level = 0
 let gameactive = false
 let lastgen = null
 
@@ -8,55 +8,43 @@ let blacklistedcolors = []
 let buttons = [];
 
 function gamefunc() {
-    for (let index = 0; index <= level; index++) {
+    level += 1
+    for (let index = 1; index <= level; index++) {
         for (let indexis = 0; indexis < 2; indexis++) {
             if (blacklistedcolors.length != colors.length) {
-
-                var whichbut = index.toString() + '-' + indexis.toString()
-                var butcolor = colors[Math.round(Math.random() * colors.length)]
                 
-                $("*").text(blacklistedcolors.find(butcolor))
-                
-                while (blacklistedcolors.find(butcolor) != null) {
-                    var butcolor = colors[Math.round(Math.random() * colors.length)]
-                }
+                var whichbut = `${index}-${indexis}`
+                var butcolor = colors[index]
 
-                var button = `<button class="from" id="button${whichbut.toString()}" onclick="from(${whichbut.toString()})" style="background-color: ${butcolor};">${index}</button>`
+                var button = `<button class="from" id="button${whichbut}" onclick="from(${whichbut})" style="background-color: ${butcolor}; position: absolute; top: ${(Math.random() * ($(window).innerHeight() - 20)) + 10}px, left: ${(Math.random() * ($(window).innerWidth() - 20)) + 10}px">${index}</button>`
                 blacklistedcolors.push(butcolor)
+                buttons.push(whichbut)
                 $("#game").append(button)
-
-
-                
-                // if (lastgen != null) {
-                //     $(lastgen).append(button)
-                // } else {
-                   
-                // }
             }
         }
 
     }
 }
 
-function start() {
-    $("button").attr("disabled", "true")
-    $("#title").transition({
-        "transform": "translateY(-150vh)"
-    }, 1500)
-    setTimeout(() => {
-        $("#title").hide(0)
-        $("#game").show(1000)
-    }, 1600);
-    gameactive = true
-    gamefunc()
-}
+// function start() {
+//     $("button").attr("disabled", "true")
+//     $("#title").transition({
+//         "transform": "translateY(-150vh)"
+//     }, 1500)
+//     setTimeout(() => {
+//         $("#title").hide(0)
+//         $("#game").show(1000)
+//     }, 1600);
+//     gameactive = true
+//     gamefunc()
+// }
 
 
 
 function from(frombutton) {
-    if (activebutton != null) {
-        console.log(frombutton.charAt(0), activebutton.charAt(0))
-    }
+    // if (activebutton != null) {
+    //     console.log(frombutton.charAt(0), activebutton.charAt(0))
+    // }
     var thebutton = $("#button" + frombutton)
     if (activebutton == null) {
         activebutton = frombutton
@@ -71,10 +59,14 @@ function from(frombutton) {
         thebutton.css("z-index", "10")
         activebutton = null
     } else if (frombutton != activebutton && frombutton.charAt(0) == activebutton.charAt(0)) {
-        thebutton.css("background-color", "limegreen !important")
-        $("#button" + activebutton).css('background-color', 'limegreen')
+        buttons.splice(buttons[buttons.indexOf(frombutton)]) 
+        buttons.splice(buttons[buttons.indexOf(activebutton)]) 
+        thebutton.css("color", "limegreen")
+        $("#button" + activebutton).css('color', 'limegreen')
         activerbutton = $("#button" + activebutton)
         activebutton = null
+        activerbutton.attr("disabled", "true")
+        thebutton.attr("disabled", "true")
         thebutton.transition({
             "transform": "scale(0)",
         }, 3000)
@@ -83,7 +75,25 @@ function from(frombutton) {
             "transform": "scale(0)",
         }, 3000)
     } else if (frombutton != activebutton && frombutton.charAt(0) != activebutton.charAt(0)) {
-        thebutton.css("background-color", "red")
+        activerbutton = $("#button" + activebutton)
+        thebutton.css("color", "red")
+        activerbutton.css("color", "red")
+        thebutton.css("animation-duration", "0.5s")
+        activerbutton.css("animation-duration", "0.5s")
+        setTimeout(() => {
+            thebutton.css("color", "white")
+            activerbutton.css("color", "white")
+            thebutton.css("animation-duration", "6s")
+            activerbutton.css("animation-duration", "6s")
+        }, 1000);
+    }
+    if (buttons.length() <= 0) {
+        setTimeout(() => {
+            if (level = 0) {
+                
+            }
+            gamefunc()
+        }, 1000);
     }
 }
 
@@ -120,5 +130,5 @@ $(document).mousemove(function (event) {
     mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
     mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
 
-    $('body').css('background', 'radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, rgb(100, 85, 120), rgb(90, 75, 110))');
+    $('body').css('background', 'radial-gradient(circle at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, rgb(75, 30, 120), rgb(80, 65, 100) 130px)');
 });
